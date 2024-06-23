afec86edad65d876bfc34822aed6d5454377ce7e
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-23 07:49:44 +0300

    05. ex done

--- 06 - Exercise Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
+++ 06 - Exercise Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -13,10 +13,10 @@ namespace SoftUni
         static void Main(string[] args)
         {
            var context = new SoftUniContext();
-            Console.WriteLine(GetEmployeesFullInformation(context));
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
         }
 
-        //01.
+        //03.
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
             var employees = context.Employees
@@ -40,6 +40,53 @@ namespace SoftUni
             return sb.ToString().TrimEnd();
         }
 
+        //04.
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
 
+            var richEmployee = context.Employees
+                .Where(e => e.Salary > 50_000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName);
+
+              var sb = new StringBuilder();
+
+            foreach (var e in richEmployee)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+        //05.
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                 .Where(e => e.Department.Name == "Research and Development")
+                 .Select(e => new
+                 {
+                     e.FirstName,
+                     e.LastName,
+                     e.Department,
+                     e.Salary
+                 })
+                 .OrderBy(e => e.Salary)
+                 .ThenByDescending(e => e.FirstName);
+
+            var sb = new StringBuilder();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.Department.Name} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
