b742138f72ebe3c47080cea8c96ef0fc939ffef1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-30 09:04:01 +0300

    02. ex create

--- 10 - Exercise LINQ/04.LINQ-Exercises-MusicHub-6.0/MusicHub/obj/Debug/net6.0/MusicHub.AssemblyInfo.cs
+++ 10 - Exercise LINQ/04.LINQ-Exercises-MusicHub-6.0/MusicHub/obj/Debug/net6.0/MusicHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed1a2d03474e4bc34f5c1b2bab31978fd18689f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa718e23db67ad6de9babec54860ebe5779ee9b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
