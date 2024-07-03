6988218c7408e3043f272385c1165d63f8e738cc
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-07-03 06:09:17 +0300

    01.ex Create

---
+++ 12 - Exercise Advanced Querying/05.Advanced-Querying-Exercises-BookShop-6.0/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=.;Database=StudentSystem;User Id=sa;Password=Project123;TrustServerCertificate=true";
+    }
+}
---
+++ 12 - Exercise Advanced Querying/05.Advanced-Querying-Exercises-BookShop-6.0/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByAgeRestriction(context, "miNor"));
+        }
+
+        //01.
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var commandToLower = command.ToLower();
+            var books = context.Books
+                .Where(b => b.Title.ToLower() == commandToLower)
+                .Select(b => b.Title)
+                .OrderBy(b => b);
+
+
+            return string.Join(Environment.NewLine,books);
+        }
+    }
+}
+
+
