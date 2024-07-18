6f090c7ea6eb0449a9d89f0f22cfee5576897aaf
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-07-18 18:15:15 +0300

    01. ex 100/100

---
+++ 16 - Exercise JSON Processing/07.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+             "Server=.;Database=ProductShop;User Id=sa;Password=Project123;TrustServerCertificate=true";
+            
+    }
+}
---
+++ 16 - Exercise JSON Processing/07.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+           ProductShopContext  context = new ProductShopContext();
+
+
+            //01.
+            string userText = File.ReadAllText("../../../Datasets/users.json");
+            Console.WriteLine(ImportUsers(context,userText));    
+
+        }
+
+        //01.
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        //02.
+
+
+    }
+}
