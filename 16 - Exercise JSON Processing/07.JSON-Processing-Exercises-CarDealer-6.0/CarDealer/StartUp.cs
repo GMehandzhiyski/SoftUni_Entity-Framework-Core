25b07b80e087f24d3a1e6f31e98f98a2b777a25b
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-07-19 07:34:07 +0300

    10. ex 100/100

--- 16 - Exercise JSON Processing/07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
+++ 16 - Exercise JSON Processing/07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿using CarDealer.Data;
 using CarDealer.Models;
+using Microsoft.EntityFrameworkCore.Update;
 using Newtonsoft.Json;
 using System.Text.Json.Serialization;
 
@@ -11,9 +12,13 @@ namespace CarDealer
         {
             CarDealerContext context = new CarDealerContext();
 
-            //01.
-            string userText = File.ReadAllText("../../../Datasets/suppliers.json");
-            Console.WriteLine(ImportSuppliers(context,userText));
+            //09.
+            //string userText = File.ReadAllText("../../../Datasets/suppliers.json");
+            //Console.WriteLine(ImportSuppliers(context,userText));
+
+            //10.
+            string userText = File.ReadAllText("../../../Datasets/parts.json");
+            Console.WriteLine(ImportParts(context, userText));
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
@@ -23,8 +28,25 @@ namespace CarDealer
             context.Suppliers.AddRange(suppliers);
             context.SaveChanges();
 
-
             return $"Successfully imported {suppliers.Count}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var parts = JsonConvert.DeserializeObject<List<Part>>(inputJson);
+
+            var validSupplierId = context.Suppliers
+                .Select(x => x.Id)
+                .ToList();
+
+            var partsImport = parts
+                .Where(p => validSupplierId.Contains(p.SupplierId))
+                .ToList();
+
+            context.Parts.AddRange(partsImport);
+            context.SaveChanges();
+
+            return $"Successfully imported {partsImport.Count}.";
         }
     }
 }
--- 16 - Exercise JSON Processing/07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/obj/Debug/net6.0/CarDealer.AssemblyInfo.cs
+++ 16 - Exercise JSON Processing/07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/obj/Debug/net6.0/CarDealer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDealer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4087141359f785ba3b0af59459b83bc1eaad05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6931b79bc3ba28c601ebad7898a7eca0fd54d95e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDealer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDealer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
