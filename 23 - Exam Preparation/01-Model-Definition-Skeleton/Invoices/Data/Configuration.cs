f031f7fe3a185a43c9c884adac104925f1b197c0
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-07-29 17:51:18 +0300

    01. 50/50

---
+++ 23 - Exam Preparation/01-Model-Definition-Skeleton/Invoices/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Invoices.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+        "Server=.;Database = InvoicesUser; Id=sa;Password=Project123;TrustServerCertificate=true";
+    }
+}
